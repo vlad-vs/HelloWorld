aaa14314a7e78e66831e6084c6f06a3f7f4d435e
vlad-vs <vlad-vs@yandex.ru>
2017-09-06 17:56:40 +0300

    init commit

---
+++ src/main/java/dataProgect/HelloWorld.java
@@ -0,0 +1,99 @@
+package dataProgect;
+
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class HelloWorld {
+
+    private static final Logger log = Logger.getLogger(HelloWorld.class);
+
+    private static int getDateHours () {
+        log.info("============== > metod: getDateHours ==> time");
+        return new Date().getHours();
+    }
+
+    public static String getLocation(){
+        if (Locale.getDefault().getDisplayLanguage().equals("русский")) {
+            log.info("============== > metod: getLocation ==> русский");
+            return "русский";
+        } else if (Locale.getDefault().getDisplayLanguage().equals("українська")) {
+            log.info("============== > metod: getLocation ==> українська");
+            return "українська";
+        } else if (Locale.getDefault().getDisplayLanguage().equals("English")) {
+            log.info("============== > metod: getLocation ==> English");
+            return "English";
+        }
+        log.error("============== > metod: getLocation ==> not found location !!!!!!");
+        return "not found";
+    }
+
+    public static ArrayList<String> locationText(String s){
+        ArrayList<String> licalList = new ArrayList<String>(4);
+        if (s.equalsIgnoreCase("English")){
+            licalList.add("Good morning, World!");
+            licalList.add("Good day, World!");
+            licalList.add("Good evening, World!");
+            licalList.add("Good night, World!");
+            log.info("============== > metod: locationText ==> English");
+        }
+        if (s.equalsIgnoreCase("русский")){
+            licalList.add("Доброе утро, Мир!");
+            licalList.add("Добрый день, Мир!");
+            licalList.add("Добрый вечер, Мир!");
+            licalList.add("Доброй ночи, Мир!");
+            log.info("============== > metod: locationText ==> русский");
+        }
+        if (s.equalsIgnoreCase("українська")){
+            licalList.add("Доброго ранку, Світ!");
+            licalList.add("Добрий день, Світ!");
+            licalList.add("Добрий вечір, Світ!");
+            licalList.add("Доброї ночі, Світ!");
+            log.info("============== > metod: locationText ==> українська");
+        }
+        if (licalList.size() == 0){
+            log.error("============== > metod: locationText ==> null List");
+        }
+        return licalList;
+    }
+
+    public static String sayHello(int  hours, ArrayList<String> licalList){
+
+        if (hours >= 6 && hours < 9) {
+            log.info("============== > metod: sayHello ==> 06:00 - 09:00");
+            return (licalList.get(0)); //06:00 - 09:00
+        }
+
+        if (hours >= 9 && hours < 19) {
+            log.info("============== > metod: sayHello ==> 09:00 - 19:00");
+            return (licalList.get(1)); //09:00 - 19:00
+        }
+
+        if (hours >= 19 && hours < 23) {
+            log.info("============== > metod: sayHello ==> 19:00 - 23:00");
+            return (licalList.get(2)); //19:00 - 23:00
+        }
+
+        if (hours >= 23 && hours < 6) {
+            log.info("============== > metod: sayHello ==> 23:00 - 06:00");
+            return (licalList.get(3)); //23:00 - 06:00
+        }
+        log.error("============== > metod: sayHello ==> null !!!!!!!!");
+        return null;
+    }
+
+
+    public static void main(String args[]) {
+
+        String location = getLocation();
+
+        ArrayList<String> locatonText = locationText(location);
+
+        int hours = getDateHours();
+
+        System.out.println(sayHello(hours, locatonText));
+
+    }
+}
---
+++ src/test/java/dataProgect/HelloWorldTest.java
@@ -0,0 +1,82 @@
+package dataProgect;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void getLocationTest() {
+        String location = HelloWorld.getLocation();
+        if (location.equals("not found")){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void locationTextEnglishTest() {
+        ArrayList<String> local = HelloWorld.locationText("English");
+        if (!local.get(0).equals("Good morning, World!")) {
+            Assert.fail();
+        }
+        if (!local.get(1).equals("Good day, World!")) {
+            Assert.fail();
+        }
+        if (!local.get(2).equals("Good evening, World!")) {
+            Assert.fail();
+        }
+        if (!local.get(3).equals("Good night, World!")) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void locationTextRusTest() {
+        ArrayList<String> local = HelloWorld.locationText("русский");
+        if (!local.get(0).equals("Доброе утро, Мир!")) {
+            Assert.fail();
+        }
+        if (!local.get(1).equals("Добрый день, Мир!")) {
+            Assert.fail();
+        }
+        if (!local.get(2).equals("Добрый вечер, Мир!")) {
+            Assert.fail();
+        }
+        if (!local.get(3).equals("Доброй ночи, Мир!")) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void locationTextUkrTest() {
+        ArrayList<String> local = HelloWorld.locationText("українська");
+        if (!local.get(0).equals("Доброго ранку, Світ!")) {
+            Assert.fail();
+        }
+        if (!local.get(1).equals("Добрий день, Світ!")) {
+            Assert.fail();
+        }
+        if (!local.get(2).equals("Добрий вечір, Світ!")) {
+            Assert.fail();
+        }
+        if (!local.get(3).equals("Доброї ночі, Світ!")) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void sayHelloTest() {
+        ArrayList<String> local = HelloWorld.locationText("русский");
+        long dateTime = 6;
+        String sayHello = HelloWorld.sayHello(6, local);
+        if (sayHello.equals("Добрый день, Мир!")){
+            Assert.fail();
+        }
+    }
+}
